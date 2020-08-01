f0639d1c260551bd3d507ee80d82742fa8dcb4e5
vkill <vkill.net@gmail.com>
2020-08-01 12:51:06 +0800

    To impl TlsClientUpgrader and TlsServerUpgrader for ()

--- src/tls.rs
+++ src/tls.rs
@@ -4,7 +4,11 @@ mod upgradable_ {
 
     pub trait TlsClientUpgrader<S>: Upgrader<S> {}
 
+    impl<S> TlsClientUpgrader<S> for () where S: Send + 'static {}
+
     pub trait TlsServerUpgrader<S>: Upgrader<S> {}
+
+    impl<S> TlsServerUpgrader<S> for () where S: Send + 'static {}
 }
 
 #[cfg(feature = "upgradable")]
